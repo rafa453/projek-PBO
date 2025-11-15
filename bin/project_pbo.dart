816c6e5307bddcea754b466a7adf92613f428b02
rafa453 <191682646+rafa453@users.noreply.github.com>
2025-11-16 00:13:47 +0700

    tambah dart_console dan intreact_cli

--- bin/project_pbo.dart
+++ bin/project_pbo.dart
@@ -1,6 +1,39 @@
-import 'package:project_pbo/project_pbo.dart' as project_pbo;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${project_pbo.calculate()}!');
+import 'package:dart_console/dart_console.dart';
+import 'package:interact_cli/interact_cli.dart';
+import 'package:project_pbo/Barang.dart';
+import 'package:project_pbo/Gudang.dart';
+import 'package:project_pbo/item_transaksi.dart';
+import 'package:project_pbo/Pembeli.dart';
+import 'package:project_pbo/Supplier.dart';
+import 'package:project_pbo/Transaksi.dart';
+
+final Console console = Console();
+
+void main() {
+  var gudangPusat = Gudang('G001', 'Gudang Utama');
+  List<Pembeli> daftarPembeli = [];
+
+  console.clearScreen();
+  console.setTextStyle(bold: true);
+
+  console.setForegroundColor(ConsoleColor.brightBlue);
+  console.writeLine(
+    '=======================================',
+    TextAlignment.center,
+  );
+  console.setForegroundColor(ConsoleColor.brightYellow);
+  console.writeLine(
+    '    APLIKASI MANAJEMEN SEMBAKO (PBO)',
+    TextAlignment.center,
+  );
+  console.setForegroundColor(ConsoleColor.brightBlue);
+  console.writeLine(
+    '=======================================',
+    TextAlignment.center,
+  );
+  console.resetColorAttributes();
+
+  
 }
-
--- test/project_pbo_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:project_pbo/project_pbo.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
